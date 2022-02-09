6c668cee12cab2c33923faf6899bf405a0a22efe
91093678 <charlesddelacruz69@gmail.com>
2022-02-09 08:25:44 -0800

    First test run

---
+++ src/AllMyPets.java
@@ -0,0 +1,8 @@
+public class AllMyPets
+{
+    public static void main (String[]args)
+    {
+        myPets testPet = new myPets(false, "food",2,"testPet");
+        System.out.println(testPet.toString());
+    }
+}
